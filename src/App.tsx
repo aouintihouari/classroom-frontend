ce9725676feeeb18b646cc5ae131bff8d1d296ad
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2026-01-18 22:58:30 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,61 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
+import {BookIcon, Home} from "lucide-react";
+
+import { Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import routerProvider, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import routerProvider, { DocumentTitleHandler, UnsavedChangesNotifier } from "@refinedev/react-router";
+
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 
+import { Layout } from "@/components/refine-ui/layout/layout.tsx";
+
+import Dashboard from "@/pages/Dashboard.tsx";
+import SubjectsList from "@/pages/subjects/SubjectsList.tsx";
+import SubjectsCreate from "@/pages/subjects/SubjectsCreate.tsx";
+
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
             <Refine
-              dataProvider={dataProvider}
-              notificationProvider={useNotificationProvider()}
-              routerProvider={routerProvider}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                projectId: "JTLUE9-DcjzjH-fDBkFA",
-              }}
-            >
+              dataProvider={ dataProvider }
+              notificationProvider={ useNotificationProvider() }
+              routerProvider={ routerProvider }
+              options={{ syncWithLocation: true, warnWhenUnsavedChanges: true, projectId: "JTLUE9-DcjzjH-fDBkFA" }}
+              resources={[
+                  {
+                      name: "dashboard",
+                      list: "/",
+                      meta: { label: "Home",  icon: <Home/> }
+                  },
+                  {
+                      name:"subjects",
+                      list:"/subjects",
+                      create: "/subjects/create",
+                      meta: { label: "Subjects",  icon: <BookIcon /> }
+                  }
+              ]
+            }>
               <Routes>
-                <Route index element={<WelcomePage />} />
+                  <Route element={
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                  }>
+                    <Route path="/" element={<Dashboard />}  />
+                    <Route path="subjects">
+                        <Route index element={<SubjectsList />} />
+                        <Route path="create" element={<SubjectsCreate />} />
+                    </Route>
+                  </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
